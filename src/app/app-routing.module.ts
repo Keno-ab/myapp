6a462831caf72a70bc5f196529ec6ee9c818c76a
Keno-ab <eu.astrosa@duocuc.cl>
2023-10-16 18:39:02 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recupera',
+    loadChildren: () => import('./recupera/recupera.module').then( m => m.RecuperaPageModule)
+  },
+  {
+    path: 'crearcuenta',
+    loadChildren: () => import('./crearcuenta/crearcuenta.module').then( m => m.CrearcuentaPageModule)
+  },
+  {
+    path: 'mapa',
+    loadChildren: () => import('./mapa/mapa.module').then( m => m.MapaPageModule)
+  },
+  {
+    path: 'disponible',
+    loadChildren: () => import('./disponible/disponible.module').then( m => m.DisponiblePageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
